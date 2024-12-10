4363e50d9109cc3ebc751d5c01fe270ae275fa99
mukt-SPEC <alaminsad6@gmail.com>
2024-12-10 10:19:28 +0100

    minor

--- lib/screen/details_page.dart
+++ lib/screen/details_page.dart
@@ -22,61 +22,124 @@ class _DetailsPageState extends State<DetailsPage> {
         forceMaterialTransparency: true,
         title: Text(
           site.placeName!,
-          style: kTextStyle(16, textWeight: TextWeight.semiBold),
+          style: kTextStyle(20, textWeight: TextWeight.semiBold),
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
+        padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
+        child: Stack(
           children: [
-            Container(
-              height: 180,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: const Color(0xfff2f2f2),
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 180,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: const Color(0xfff2f2f2),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            site.address!,
+                            style:
+                                kTextStyle(14, textWeight: TextWeight.normal),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.star,
+                            color: Color.fromARGB(255, 168, 152, 10),
+                          ),
+                          const SizedBox(
+                            width: 4,
+                          ),
+                          Text(
+                            site.rating.toString(),
+                            style:
+                                kTextStyle(14, textWeight: TextWeight.semiBold),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    'Description',
+                    style: kTextStyle(16, textWeight: TextWeight.semiBold),
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    site.description!,
+                    style: kTextStyle(12),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    'Location',
+                    style: kTextStyle(16, textWeight: TextWeight.semiBold),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      color: const Color(0xfff2f2f2),
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(
+                        width: 1.5,
+                        color: const Color(0xff121212),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(
-              height: 12,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      site.address!,
-                      style: kTextStyle(14, textWeight: TextWeight.normal),
+            Positioned(
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 48,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
                     ),
-                  ],
+                    onPressed: () {},
+                    child: Text(
+                      'Get Direction',
+                      style: kTextStyle(16,
+                          textWeight: TextWeight.semiBold,
+                          color: const Color(0xffffffff)),
+                    ),
+                  ),
                 ),
-                Row(
-                  children: [
-                    const Icon(
-                      Icons.star,
-                      color: Color.fromARGB(255, 168, 152, 10),
-                    ),
-                    const SizedBox(
-                      width: 4,
-                    ),
-                    Text(
-                      site.rating.toString(),
-                      style: kTextStyle(14, textWeight: TextWeight.semiBold),
-                    ),
-                  ],
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Text(
-              site.description!,
-              style: kTextStyle(12),
-            ),
+              ),
+            )
           ],
         ),
       ),
